1cb898401feb371d3e86211b0fe8660835be4ed1
I326319 <ranganath.sn@gmail.com>
2019-08-20 02:19:06 +0530

    hello world spring mvc

---
+++ src/main/java/com/snr/controller/HelloController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.snr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author I326319
+ *
+ */
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value="/greeting")
+	public String sayHello(Model model) {
+		model.addAttribute("greeting", "Hello World");
+		return "hello";
+	}
+}
